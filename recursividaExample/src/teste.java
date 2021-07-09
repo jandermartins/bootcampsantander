bb1f990f646c8a5684164d762cc50c099d7082df
Jander Martins <jandermartins@alu.ufc.br>
2021-07-09 16:03:34 -0300

    aula recursividade

---
+++ recursividaExample/src/teste.java
@@ -0,0 +1,16 @@
+public class teste {
+
+    public static void main(String[] args){
+
+        System.out.println(fatorial(5));
+
+
+
+    }
+
+    public static int fatorial(int numero){
+        if(numero == 0) {
+            return numero;
+        }return numero*fatorial(numero--);
+    }
+}

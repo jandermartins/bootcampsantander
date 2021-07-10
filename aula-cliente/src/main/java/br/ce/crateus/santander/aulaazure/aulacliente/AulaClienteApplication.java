82349b6cfba22aed282d6e17f5a1de9c94e38e43
Jander Martins <jandermartins@alu.ufc.br>
2021-07-10 12:06:53 -0300

    atualizações

---
+++ aula-cliente/src/main/java/br/ce/crateus/santander/aulaazure/aulacliente/AulaClienteApplication.java
@@ -0,0 +1,13 @@
+package br.ce.crateus.santander.aulaazure.aulacliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaClienteApplication.class, args);
+	}
+
+}
---
+++ aula-cliente/src/test/java/br/ce/crateus/santander/aulaazure/aulacliente/AulaClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ce.crateus.santander.aulaazure.aulacliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e197db7b9f5dd97401ea6066ece89ac92b9de22d
Jander Martins <jandermartins@alu.ufc.br>
2021-08-05 21:42:44 -0300

    atualizando pasta

---
+++ novo/controledeponto/src/main/java/br/com/dio/controledeponto/model/ControleUsuario.java
@@ -0,0 +1,10 @@
+package br.com.dio.controledeponto.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "controle_usuario")
+public class ControleUsuario {
+
+
+}
